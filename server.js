c44b79d6975870a0c96b0edb02e9c66192ccd058
Florian Wugeditsch <airkrypton@gmx.at>
2023-09-03 22:27:03 +0200

    update

--- server.js
+++ server.js
@@ -2,60 +2,11 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-// Importiere das ORM und die Datenbankverbindung (z.B., Sequelize oder Mongoose)
-
-// Middleware für JSON-Anfragen
-app.use(express.json());
-
-// API-Endpunkt zum Hinzufügen eines neuen Gerichts
-app.post('/api/dishes', async (req, res) => {
-  try {
-    // Code zum Hinzufügen des Gerichts in der Datenbank
-    // Beispiel: await Dish.create(req.body);
-    res.status(201).send('Gericht wurde hinzugefügt');
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Fehler beim Hinzufügen des Gerichts');
-  }
-});
-
-// API-Endpunkt zum Abrufen aller Gerichte
-app.get('/api/dishes', async (req, res) => {
-  try {
-    // Code zum Abrufen aller Gerichte aus der Datenbank
-    // Beispiel: const dishes = await Dish.findAll();
-    res.json(dishes);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Fehler beim Abrufen der Gerichte');
-  }
-});
-
-// API-Endpunkt zum Aktualisieren eines Gerichts
-app.put('/api/dishes/:id', async (req, res) => {
-  try {
-    // Code zum Aktualisieren des Gerichts in der Datenbank
-    // Beispiel: await Dish.update(req.body, { where: { id: req.params.id } });
-    res.send('Gericht wurde aktualisiert');
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Fehler beim Aktualisieren des Gerichts');
-  }
-});
-
-// API-Endpunkt zum Löschen eines Gerichts
-app.delete('/api/dishes/:id', async (req, res) => {
-  try {
-    // Code zum Löschen des Gerichts aus der Datenbank
-    // Beispiel: await Dish.destroy({ where: { id: req.params.id } });
-    res.send('Gericht wurde gelöscht');
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Fehler beim Löschen des Gerichts');
-  }
-});
+// Statische Dateien aus dem aktuellen Verzeichnis bereitstellen
+app.use(express.static(__dirname));
 
 // Server starten
 app.listen(port, () => {
   console.log(`Der Server läuft auf Port ${port}`);
 });
+
